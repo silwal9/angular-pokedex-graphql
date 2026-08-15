f71295c1a9accfafb19035e1bbdd6451b84048cb
Prayas Silwal <silwal.prayas@gmail.com>
2026-08-15 21:00:20 +0545

    perf(core): tree-shake echarts modules in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { provideEchartsCore } from 'ngx-echarts';
-import * as echarts from 'echarts';
+import * as echarts from 'echarts/core';
+import { RadarChart } from 'echarts/charts';
+import { RadarComponent, TooltipComponent } from 'echarts/components';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([RadarChart, RadarComponent, TooltipComponent, CanvasRenderer]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
